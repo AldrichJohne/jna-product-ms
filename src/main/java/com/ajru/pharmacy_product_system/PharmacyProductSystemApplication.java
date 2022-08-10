85b03ef767269d9401188a2ed04fa8dfa47c7bbb
Aldrich Johne Ulabo <you@example.com>
2022-08-10 10:14:56 +0800

    add version to 1.0.0 and remove hardcoded code to add credentials

--- src/main/java/com/ajru/pharmacy_product_system/PharmacyProductSystemApplication.java
+++ src/main/java/com/ajru/pharmacy_product_system/PharmacyProductSystemApplication.java
@@ -19,25 +19,25 @@ import java.util.stream.Stream;
 @EnableWebSecurity
 @EnableConfigurationProperties(ConfigProps.class)
 public class PharmacyProductSystemApplication {
-	@Value("${jna.userName}")
-	private String userName;
-	@Value("${jna.password}")
-	private String password;
-	@Value("${jna.role}")
-	private String role;
-	@Value("${jna.id}")
-	private int id;
-
-	@Autowired
-	private UserRepository userRepository;
-
-	@PostConstruct
-	public void initUser() {
-		List<User> users = Stream.of(
-				new User(id, userName, password, "", role)
-		).collect(Collectors.toList());
-		userRepository.saveAll(users);
-	}
+//	@Value("${jna.userName}")
+//	private String userName;
+//	@Value("${jna.password}")
+//	private String password;
+//	@Value("${jna.role}")
+//	private String role;
+//	@Value("${jna.id}")
+//	private int id;
+//
+//	@Autowired
+//	private UserRepository userRepository;
+//
+//	@PostConstruct
+//	public void initUser() {
+//		List<User> users = Stream.of(
+//				new User(id, userName, password, "", role)
+//		).collect(Collectors.toList());
+//		userRepository.saveAll(users);
+//	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(PharmacyProductSystemApplication.class, args);
