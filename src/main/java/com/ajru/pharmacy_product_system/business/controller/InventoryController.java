c7260e30cedac5fead6ae3011f6a6235ff1f5239
Aldrich Johne Ulabo <you@example.com>
2023-04-14 14:15:18 +0800

    enhance the for loop used

--- src/main/java/com/ajru/pharmacy_product_system/business/controller/InventoryController.java
+++ src/main/java/com/ajru/pharmacy_product_system/business/controller/InventoryController.java
@@ -106,8 +106,8 @@ public class InventoryController {
     public ResponseEntity<List<ProductDto>> getProducts() {
         List<Product> products = productService.getProducts();//store products
         List<ProductDto> productsDto = products.stream().map(ProductDto::from).collect(Collectors.toList());//convert products to productsDto
-        for (int index = 0; index < productsDto.size(); index ++) {
-            productsDto.get(index).setClassName(productsDto.get(index).getPlainClassificationDto().getName());
+        for (ProductDto productDto : productsDto) {
+            productDto.setClassName(productDto.getPlainClassificationDto().getName());
         }
         return new ResponseEntity<>(productsDto, HttpStatus.OK);
     }
